724982b25e5d686c406931cc35d8c2c732d75537
Alex Lachance <alachance@rcginc-us.com>
2021-11-28 17:45:30 -0500

    calendar

--- src/router/index.js
+++ src/router/index.js
@@ -8,98 +8,108 @@ import Create from "../views/Create.vue";
 import ViewWorkout from "../views/ViewWorkout.vue";
 import Metrics from "../views/UserMetrics.vue";
 import CalorieRestrictions from "../views/CalorieRestrictions.vue"
+import Calendar from "../views/Calendar.vue"
 
 
 const routes = [
 
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-    meta: {
-      title: "Home",
-      auth: false,
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+        meta: {
+            title: "Home",
+            auth: false,
+        },
     },
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-    meta: {
-      title: "Login",
-      auth: false,
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+        meta: {
+            title: "Login",
+            auth: false,
+        },
     },
-  },
-  {
-    path: "/usermetrics",
-    name: "UserMetrics",
-    component: Metrics,
-    meta: {
-      title: "Login",
-      auth: false
+    {
+        path: "/usermetrics",
+        name: "UserMetrics",
+        component: Metrics,
+        meta: {
+            title: "Login",
+            auth: false
+        },
     },
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register,
-    meta: {
-      title: "Register",
-      auth: false,
+    {
+        path: "/register",
+        name: "Register",
+        component: Register,
+        meta: {
+            title: "Register",
+            auth: false,
+        },
     },
-  },
-  {
-    path: "/create",
-    name: "Create",
-    component: Create,
-    meta: {
-      title: "Create",
-      auth: true,
+    {
+        path: "/create",
+        name: "Create",
+        component: Create,
+        meta: {
+            title: "Create",
+            auth: true,
+        },
     },
-  },
-  {
-    path: "/view-workout/:workoutId",
-    name: "View-Workout",
-    component: ViewWorkout,
-    meta: {
-      title: "View Workout",
-      auth: true,
+    {
+        path: "/view-workout/:workoutId",
+        name: "View-Workout",
+        component: ViewWorkout,
+        meta: {
+            title: "View Workout",
+            auth: true,
+        },
     },
-  },
-  {
-    path: "/calorie-restrictions",
-    name: "restrictions",
-    component: CalorieRestrictions,
-    meta: {
-      title: "Calorie Restrictions",
-      auth: true,
+    {
+        path: "/calorie-restrictions",
+        name: "restrictions",
+        component: CalorieRestrictions,
+        meta: {
+            title: "Calorie Restrictions",
+            auth: true,
+        },
     },
-  },
+    {
+        path: "/calendar",
+        name: "calendar",
+        component: Calendar,
+        meta: {
+            title: "Calendar",
+            auth: true,
+        },
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 // Change document titles
 router.beforeEach((to, from, next) => {
-  document.title = `${to.meta.title} | Active Tracker`;
-  next();
+    document.title = `${to.meta.title} | Active Tracker`;
+    next();
 });
 
 // Route guard for auth routes
 router.beforeEach((to, from, next) => {
-  const user = supabase.auth.user();
-  if (to.matched.some((res) => res.meta.auth)) {
-    if (user) {
-      next();
-      return;
+    const user = supabase.auth.user();
+    if (to.matched.some((res) => res.meta.auth)) {
+        if (user) {
+            next();
+            return;
+        }
+        next({ name: "Login" });
+        return;
     }
-    next({ name: "Login" });
-    return;
-  }
-  next();
+    next();
 });
 
 export default router;
---
+++ src/views/Calendar.vue
@@ -0,0 +1,64 @@
+<template>
+    <section>
+        <div class="max-w-screen-md mx-auto px-4 py-10">
+            <label>Select a date</label>
+            <div class="max-w-screen-md mx-auto px-4 py-10">
+                <flat-pickr
+                    v-model="date"
+                    :config="config"
+                    class="form-control"
+                    placeholder="Select date"
+                    name="date"/>
+                <div class="input-group-append">
+                    <button class="btn btn-default" type="button" title="Toggle" data-toggle>
+                        <i class="fa fa-calendar">
+                            <span aria-hidden="true" class="sr-only">Toggle</span>
+                        </i>
+                    </button>
+                    <button class="btn btn-default" type="button" title="Clear" data-clear>
+                        <i class="fa fa-times">
+                            <span aria-hidden="true" class="sr-only">Clear</span>
+                        </i>
+                    </button>
+                </div>
+            </div>
+        </div>
+		<div class="max-w-screen-md mx-auto px-4 py-10">
+        <pre>Selected date is - {{date}}</pre>
+		</div>
+    </section>
+</template>
+
+<script>
+    // bootstrap is just for this example
+    import 'bootstrap/dist/css/bootstrap.css';
+    // import this component
+    import flatPickr from 'vue-flatpickr-component';
+    import 'flatpickr/dist/flatpickr.css';
+    // theme is optional
+    // try more themes at - https://flatpickr.js.org/themes/
+    import 'flatpickr/dist/themes/material_blue.css';
+    // localization is optional
+    //import {English} from 'flatpickr/dist/l10n/hi.js';
+
+    export default {
+        name: 'calendar',
+        data() {
+            return {
+                // Initial value can be a date object as well
+                date: '2020-10-16',
+                // Get more form https://flatpickr.js.org/options/
+                config: {
+                    wrap: true, // set wrap to true only when using 'input-group'
+                    altFormat: 'M j, Y',
+                    altInput: true,
+                    dateFormat: 'Y-m-d'
+                    //          
+                },
+            }
+        },
+        components: {
+            flatPickr
+        },
+    }
+</script>
