8bdf57e2d270d110d12018a8f423db0e2da7e074
Alex Lachance <alachance@rcginc-us.com>
2021-11-28 17:31:21 -0500

    broken

---
+++ src/views/Calendar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <flat-pickr v-model="date"></flat-pickr>
+    </div>
+</template>
+
+<script>
+import flatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
+
+export default {
+	name: 'calendar',
+	setup() {
+		return {
+			date: null,
+		}
+	},
+	components: {
+		flatPickr
+	}
+}
+</script>
