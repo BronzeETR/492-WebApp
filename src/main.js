a06fb3d191b6217b3143536312f84b4d59543200
Alex Lachance <alachance@rcginc-us.com>
2021-11-29 22:22:15 -0500

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import VCalendar from 'v-calendar';
 import router from "./router";
 import "./assets/tailwind.css";
 
 createApp(App)
-  .use(router)
-  .mount("#app");
+    .use(router)
+    .use(VCalendar, {})
+    .mount("#app");
